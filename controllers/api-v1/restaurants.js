392fbd97161d545cfe377dd80ac099d3a121b8fe
bluz225 <bluz225@gmail.com>
2022-11-01 20:23:54 -0700

    📦 NEW: add new restaurant functionality added

--- controllers/api-v1/restaurants.js
+++ controllers/api-v1/restaurants.js
@@ -18,9 +18,19 @@ router.get("/", async (req, res) => {
 
 const yelpAPI = require('../../services/yelpAPI')
 router.get("/yelpSearch", async (req,res)=>{
+    // ?search=:term&lat=:lat&long=:long&address=:address
     try {
         // console.log(req.body)
-        const results = await yelpAPI.yelpAPIsearch(req.body)
+        console.log(req.query)
+        const reqbody = {
+            searchTerm: req.query.search,
+            coordinates:{
+                lat:req.query.lat,
+                long:req.query.long
+            },
+            location:req.query.address
+        }
+        const results = await yelpAPI.yelpAPIsearch(reqbody)
         res.status(200).json(results)
     } catch (error) {
         console.warn(error)
@@ -34,6 +44,46 @@ router.get("/:id", async (req, res) => {
         const oneRest = await db.Restaurant.findById(req.params.id)
         .populate("menu")
         res.status(200).json(oneRest)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json(error)
+    }
+})
+
+const {
+    createNewRest,
+    addHours,
+    addMainMenu,
+    addFoodMenu,
+    addDrinkMenu,
+    addCusine
+} = require('../../functions/createNewRest.js')
+
+router.post("/newRestaurant", async (req,res) => {
+    try {       
+        const createdRest = await createNewRest(req.body.restaurantData)
+        // console.log(createdRest)
+        await addHours(createdRest, req.body.restaurantData.hours)
+        const createdMainMenu = await addMainMenu(createdRest, req.body.restaurantData.menu)
+        await addFoodMenu(createdMainMenu,req.body.restaurantData.menu.foodMenu)
+        await addDrinkMenu(createdMainMenu,req.body.restaurantData.menu.drinkMenu)
+        // const newRest = await db.Restaurant.create({
+        //     yelpRestaurantId, name,
+        //     telNumber,
+        //     cuisines,
+        //     displayNumber,
+        //     address1,
+        //     address2,
+        //     address3,
+        //     city,
+        //     zip_code,
+        //     country,
+        //     longitude,
+        //     latitude,
+        //     image_url
+        // })
+        // console.log(newRest)
+        res.status(200).json({msg:"yep"})
     } catch (error) {
         console.log(error)
         res.status(400).json(error)
---
+++ functions/createNewRest.js
@@ -0,0 +1,118 @@
+const db = require("../models")
+
+async function createNewRest(restaurantData) {
+    // console.log("hits create new rest function module export")
+    // console.log(restaurantData)
+    // const newRestResponse = await db.Restaurant.create({restaurantData})
+    const {
+        yelpRestaurantId,
+        name,
+        telNumber,
+        displayNumber,
+        address1,
+        address2,
+        address3,
+        city,
+        zip_code,
+        country,
+        longitude,
+        latitude,
+        image_url,
+        hasDrinks,
+        hasFood,
+        dogFriendly,
+        hasPatio
+    } = restaurantData
+    
+    const newRestResponse = await db.Restaurant.findOneAndUpdate({
+        yelpRestaurantId
+        },{
+            name,
+            telNumber,
+            displayNumber,
+            address1,
+            address2,
+            address3,
+            city,
+            zip_code,
+            country,
+            longitude,
+            latitude,
+            image_url,
+            hasDrinks,
+            hasFood,
+            dogFriendly,
+            hasPatio
+        },{
+        upsert: true,
+        new: true
+    })
+
+    return newRestResponse
+}
+
+async function addHours(restaurantObject, HoursArr) {
+    HoursArr.forEach((hhHour) => {
+        restaurantObject.hours.push(hhHour)
+    })
+    await restaurantObject.save()
+    return 
+}
+
+async function addMainMenu(restaurantObject, menuObj) {
+    
+    // console.log(restaurantObject)
+    // console.log(menuObj)
+    const {
+        restaurantName,
+        isChain,
+        hasFoodSpecials,
+        foodSpecialsDescriptions,
+        hasDrinkSpecials,
+        drinkSpecialsDescriptions
+    } = menuObj
+       
+    const newMenu = await db.Menu.create({
+        restaurantName,
+        isChain,
+        hasFoodSpecials,
+        foodSpecialsDescriptions: menuObj.foodSpecialsDescriptions,
+        hasDrinkSpecials,
+        drinkSpecialsDescriptions: menuObj.drinkSpecialsDescriptions,
+        restaurantName:restaurantObject.name,
+    })
+    restaurantObject.menu = newMenu
+    newMenu.restaurant.push(restaurantObject)
+    await restaurantObject.save()
+    await newMenu.save()
+    return newMenu
+}
+
+async function addFoodMenu(mainMenuObj, FoodMenuArr) {
+    console.log("hits Food Menu Function")
+    FoodMenuArr.forEach((item)=>{
+        mainMenuObj.foodMenu.push(item)
+    })
+    return await mainMenuObj.save()
+}
+
+async function addDrinkMenu(mainMenuObj, DrinkMenuArr) {
+    console.log("hits Drink Menu Function")
+    DrinkMenuArr.forEach((item)=>{
+        mainMenuObj.drinkMenu.push(item)
+    })
+    return await mainMenuObj.save()
+}
+
+async function addCusine() {
+
+}
+
+module.exports = {
+    createNewRest,
+    addHours,
+    addMainMenu,
+    addFoodMenu,
+    addDrinkMenu,
+    addCusine
+}

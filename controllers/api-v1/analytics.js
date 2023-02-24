7e1beaaa6549d54989d7fce82d5f2efb0be808c2
Billy <bluz225@gmail.com>
2023-02-23 18:24:47 -0800

    working on analytics

--- controllers/api-v1/analytics.js
+++ controllers/api-v1/analytics.js
@@ -24,4 +24,27 @@ router.get("/RestaurantsPerCity", async (req,res)=>{
     }
 })
 
+router.get("/dailyVistors", async (req,res)=>{
+  try {
+      console.log("dailyVistors EndPoint Hit")
+      // const cityCount = await db.Restaurant.aggregate([
+      //     {
+      //       '$group': {
+      //         '_id': '$city', 
+      //         'numRestaurants': {
+      //           '$count': {}
+      //         }
+      //       }
+      //     }, {
+      //       '$sort': {
+      //         'numRestaurants': -1
+      //       }
+      //     }
+      //   ])
+      res.status(200).send()
+  } catch (error) {
+      res.status(400).json(error)
+  }
+})
+
 module.exports = router

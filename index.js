116c47c59eafaeef6b50667f87ef6556d8eea96d
Billy <bluz225@gmail.com>
2023-05-22 17:47:00 -0700

    fix2

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ async function expressMiddleware(req, res, next) {
 			newPageVist = await db.PageVisit.create({
 				ipAddress: RequestIp.getClientIp(req),
 				OS:req.query.OS ?? null,
-				Mobile: req.query.mobile ?? null,
+				Mobile: req.query.mobile ?? true,
 				Browser: req.query.browser ?? null,
 				uad: req.query.uad ?? null,
 				screenWidth: Number(req.query.screenWidth),
